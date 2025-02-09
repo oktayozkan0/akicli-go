0f2f2492f1af629a96f3a234cf1cd4ea9bb4f82f
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2025-02-10 01:04:08 +0300

    added more functionality

--- api/apps.go
+++ api/apps.go
@@ -2,14 +2,22 @@ package api
 
 import (
 	"encoding/json"
+	"strconv"
 
 	"github.com/oktayozkan0/akicli-go/utils"
 )
 
-type Application struct {
-	Pk   int    `json:"pk"`
+type ApplicationType struct {
 	Name string `json:"name"`
 	Slug string `json:"slug"`
+}
+
+type Application struct {
+	Pk              int             `json:"pk"`
+	Name            string          `json:"name"`
+	Slug            string          `json:"slug"`
+	AppType         string          `json:"app_type"`
+	ApplicationType ApplicationType `json:"application_type"`
 }
 
 type Applications struct {
@@ -27,6 +35,21 @@ func (a *API) GetApps(params map[string]string) (*Applications, error) {
 		return nil, err
 	}
 	var response Applications
+	err = json.Unmarshal(body, &response)
+	return &response, err
+}
+
+func (a *API) GetApp(id int) (*Application, error) {
+	u := "/applications/" + strconv.Itoa(id) + "/"
+	app, err := a.client.Get(u, nil)
+	if err != nil {
+		return nil, err
+	}
+	body, err := utils.ResponseAsBytes(app)
+	if err != nil {
+		return nil, err
+	}
+	var response Application
 	err = json.Unmarshal(body, &response)
 	return &response, err
 }
