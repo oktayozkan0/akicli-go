191aef3c31730b842a0f557375bffa8a83ffa63e
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2025-02-09 22:33:37 +0300

    client extended

--- client/client.go
+++ client/client.go
@@ -1,6 +1,7 @@
 package client
 
 import (
+	"fmt"
 	"net/http"
 	"time"
 )
@@ -25,12 +26,38 @@ func WithToken(token string) ClientOption {
 	}
 }
 
-func NewClient(options ...ClientOption) *Client {
+func WithTimeout(timeout time.Duration) ClientOption {
+	return func(c *Client) {
+		c.httpClient.Timeout = timeout
+	}
+}
+
+func NewClient(options ...ClientOption) (*Client, error) {
 	client := &Client{
 		httpClient: &http.Client{Timeout: 30 * time.Second},
 	}
 	for _, option := range options {
 		option(client)
 	}
-	return client
+	if client.baseURL == "" {
+		return nil, fmt.Errorf("WithBaseURL required")
+	}
+	return client, nil
+}
+
+func (c *Client) Do(req *http.Request) (*http.Response, error) {
+	if c.token != "" {
+		req.Header.Set("Authorization", c.token)
+	} else {
+		return nil, fmt.Errorf("credentials required, you must login or provide a token")
+	}
+	return c.httpClient.Do(req)
+}
+
+func (c *Client) Get(path string) (*http.Response, error) {
+	req, err := http.NewRequest("GET", c.baseURL+path, nil)
+	if err != nil {
+		return nil, err
+	}
+	return c.Do(req)
 }
