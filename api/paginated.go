690aa8956be8eacdec29222b9791a388a79ee6fc
oktayozkan0 <103560387+oktayozkan0@users.noreply.github.com>
2025-02-25 02:19:31 +0300

    generic pagination

--- api/paginated.go
+++ api/paginated.go
@@ -4,5 +4,5 @@ type PaginatedResponse[T any] struct {
 	Count    int    `json:"count"`
 	Next     string `json:"next"`
 	Previous string `json:"previous"`
-	Results  T      `json:"results"`
+	Results  []T    `json:"results"`
 }
